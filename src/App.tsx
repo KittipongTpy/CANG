226783dd288640f65e1c47f91799ef065c61833c
Kittipong Tapyou <kittipong.tpy@gmail.com>
2025-04-17 14:46:22 +0700

    init

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { Route, Routes } from "react-router-dom";
+
+import IndexPage from "@/pages/index";
+import DocsPage from "@/pages/docs";
+import PricingPage from "@/pages/pricing";
+import BlogPage from "@/pages/blog";
+import AboutPage from "@/pages/about";
+import CanvasPage from "@/pages/canvas";
+
+function App() {
+  return (
+    <Routes>
+      <Route element={<IndexPage />} path="/" />
+      <Route element={<DocsPage />} path="/docs" />
+      <Route element={<PricingPage />} path="/pricing" />
+      <Route element={<BlogPage />} path="/blog" />
+      <Route element={<AboutPage />} path="/about" />
+      <Route element={<CanvasPage />} path="/canvas" />
+
+    </Routes>
+  );
+}
+
+export default App;
