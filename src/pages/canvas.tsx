5d5cd5feca957e5c9796b87be26c35f3ad67049b
Manassanan Nilruang <manatsanan.ninrueang@gmail.com>
2025-05-15 03:07:55 +0700

    fix: select shape and after that code selected

--- src/pages/canvas.tsx
+++ src/pages/canvas.tsx
@@ -6,15 +6,17 @@ import {
   CardBody,
   CardFooter,
   Button,
-  Tabs, 
+  Tabs,
   Tab,
   ButtonGroup,
   Input,
   Switch,
-  Kbd
+  Kbd,
 } from "@heroui/react";
 import { useEffect, useState } from "react";
 import { vscodeDark } from "@uiw/codemirror-theme-vscode";
+import { EditorView, Decoration, DecorationSet } from "@codemirror/view";
+import { RangeSetBuilder } from "@codemirror/state";
 import CodeMirror from "@uiw/react-codemirror";
 import { FaRegCircle } from "react-icons/fa";
 import { TfiLayoutLineSolid } from "react-icons/tfi";
@@ -30,6 +32,7 @@ import { executeCommand } from "@/command/render";
 import { shapeToCommand } from "@/shapes/shapeToCommand";
 import ShapeCard from "@/components/shapeCard";
 import CustomShape from "@/components/customShape";
+import "./customGutter.css";
 export interface Shape {
   shape: "line" | "rectangle" | "circle" | "ellipse" | "bezier" | "hermite";
   controlPoints: { x: number; y: number }[];
@@ -45,11 +48,16 @@ export default function App() {
   const [frame, setFrame] = useState<{ x: number; y: number } | null>(null);
   const [codeCommand, setCodeCommand] = useState<string>("");
   const [errorMessage, setErrorMessage] = useState<string | null>(null);
-  const [shape, setShape] = useState<"mouse" | "line" | "rectangle" | "circle" | "ellipse" | "bezier" | "hermite">("mouse");
+  const [shape, setShape] = useState<
+    "mouse" | "line" | "rectangle" | "circle" | "ellipse" | "bezier" | "hermite"
+  >("mouse");
   const [fx, setFx] = useState<number>(1000);
   const [fy, setFy] = useState<number>(1000);
   const [grid, setGrid] = useState<boolean>(true);
-  const [mousePos, setMousePos] = useState<{ x: number; y: number }>({ x: 0, y: 0 });
+  const [mousePos, setMousePos] = useState<{ x: number; y: number }>({
+    x: 0,
+    y: 0,
+  });
   const [renderData, setRenderData] = useState<Shape[]>([]);
   const [shapeId, setShapeId] = useState<number | null>(null);
 
@@ -74,7 +82,7 @@ export default function App() {
       return;
     }
     const restCommand = lines.slice(1).join("\n");
-    const {drawData, errors } = executeCommand(restCommand);
+    const { drawData, errors } = executeCommand(restCommand);
     console.log("error:", errors);
 
     if (errors.length > 0 && errors[0] !== " ") {
@@ -86,12 +94,14 @@ export default function App() {
     setCodeCommand(restCommand);
     const convertedShapes: Shape[] = drawData.map((item) => {
       return {
-      shape: item.type,
-      controlPoints: (item.controlPoints ?? item.points.slice(0, 2)).map(([x, y]) => ({ x, y })),
-      color: item.color ?? "#808080",
-      isFilled: false,
-      strokeWidth: item.strokeWidth ?? 1,
-      points: item.points.map(([x, y]) => ({ x, y })),  
+        shape: item.type,
+        controlPoints: (item.controlPoints ?? item.points.slice(0, 2)).map(
+          ([x, y]) => ({ x, y })
+        ),
+        color: item.color ?? "#808080",
+        isFilled: false,
+        strokeWidth: item.strokeWidth ?? 1,
+        points: item.points.map(([x, y]) => ({ x, y })),
       };
     });
     setRenderData(convertedShapes);
@@ -104,7 +114,6 @@ export default function App() {
 
   useEffect(() => {
     setFrame({ x: fx, y: fy });
-
   }, [fx, fy]);
 
   useEffect(() => {
@@ -113,7 +122,22 @@ export default function App() {
     const commands = renderData.map(shapeToCommand).join("\n");
     const fullCode = `INIT ${fx} ${fy}\n${commands}`;
     setCode(fullCode);
-  }, [renderData, fx, fy,isUserEditingCode]);
+  }, [renderData, fx, fy, isUserEditingCode]);
+
+  function highlightLines(lineIndexes: number[]) {
+    return EditorView.decorations.compute([], (state) => {
+      const builder = new RangeSetBuilder<Decoration>();
+      lineIndexes.forEach((lineIndex) => {
+        const line = state.doc.line(lineIndex + 1); // Line numbers are 1-based
+        builder.add(
+          line.from,
+          line.from, // Add decoration to the start of the line
+          Decoration.line({ class: "highlight-line" })
+        );
+      });
+      return builder.finish();
+    });
+  }
 
   return (
     <DefaultLayout>
@@ -156,29 +180,75 @@ export default function App() {
                     />
                   </div>
                   <div className="flex items-center">
-                    Position : <Kbd>{mousePos.x.toFixed(0)}, {mousePos.y.toFixed(0)}</Kbd>
+                    Position :{" "}
+                    <Kbd>
+                      {mousePos.x.toFixed(0)}, {mousePos.y.toFixed(0)}
+                    </Kbd>
                   </div>
                   <div className="flex items-center">
-                    <Button onPress={() => setRenderData((prev) => prev.slice(0, -1))}>Back</Button>
+                    <Button
+                      onPress={() => setRenderData((prev) => prev.slice(0, -1))}
+                    >
+                      Back
+                    </Button>
                   </div>
                 </div>
                 <ButtonGroup>
-                  <Button onPress={() => setShape("mouse")} isDisabled={shape === "mouse"}><FaMousePointer /></Button>
-                  <Button onPress={() => setShape("circle")} isDisabled={shape === "circle"}><FaRegCircle />Circle</Button>
-                  <Button onPress={() => setShape("line")} isDisabled={shape === "line"}><TfiLayoutLineSolid />Line</Button>
-                  <Button onPress={() => setShape("rectangle")} isDisabled={shape === "rectangle"}><MdOutlineRectangle />Rectangle</Button>
-                  <Button onPress={() => setShape("ellipse")} isDisabled={shape === "ellipse"}><FaRegCircle />Ellipse</Button>
-                  <Button onPress={() => setShape("bezier")} isDisabled={shape === "bezier"}><PiBezierCurve />Bezier</Button>
-                  <Button onPress={() => setShape("hermite")} isDisabled={shape === "hermite"}><PiBezierCurve />Hermite</Button>
+                  <Button
+                    onPress={() => setShape("mouse")}
+                    isDisabled={shape === "mouse"}
+                  >
+                    <FaMousePointer />
+                  </Button>
+                  <Button
+                    onPress={() => setShape("circle")}
+                    isDisabled={shape === "circle"}
+                  >
+                    <FaRegCircle />
+                    Circle
+                  </Button>
+                  <Button
+                    onPress={() => setShape("line")}
+                    isDisabled={shape === "line"}
+                  >
+                    <TfiLayoutLineSolid />
+                    Line
+                  </Button>
+                  <Button
+                    onPress={() => setShape("rectangle")}
+                    isDisabled={shape === "rectangle"}
+                  >
+                    <MdOutlineRectangle />
+                    Rectangle
+                  </Button>
+                  <Button
+                    onPress={() => setShape("ellipse")}
+                    isDisabled={shape === "ellipse"}
+                  >
+                    <FaRegCircle />
+                    Ellipse
+                  </Button>
+                  <Button
+                    onPress={() => setShape("bezier")}
+                    isDisabled={shape === "bezier"}
+                  >
+                    <PiBezierCurve />
+                    Bezier
+                  </Button>
+                  <Button
+                    onPress={() => setShape("hermite")}
+                    isDisabled={shape === "hermite"}
+                  >
+                    <PiBezierCurve />
+                    Hermite
+                  </Button>
                 </ButtonGroup>
               </div>
             </CardHeader>
             <CardBody className="overflow-visible py-2 h-full">
               <ScrollShadow className="w-full h-full">
                 <div className="items-center justify-center h-full">
-
                   {frame && (
-
                     <FrameComponent
                       bgColor="#FFFFFF"
                       drawData={codeCommand}
@@ -192,7 +262,6 @@ export default function App() {
                       setRenderData={setRenderData}
                       id={shapeId}
                       setId={setShapeId}
-
                     />
                   )}
                   {!frame && (
@@ -221,9 +290,10 @@ export default function App() {
                   )}
                 </CardHeader>
                 <CardBody className="overflow-visible py-2 h-full space-y-1">
-
                   {renderData.map((shape, index) => (
-                    <ShapeCard key={index} {...shape}
+                    <ShapeCard
+                      key={index}
+                      {...shape}
                       renderData={renderData}
                       setRenderData={setRenderData}
                       shape={shape}
@@ -233,9 +303,7 @@ export default function App() {
                     />
                   ))}
                 </CardBody>
-                <CardFooter className="bg-white/10 bottom-0 border-t-10 flex justify-between">
-
-                </CardFooter>
+                <CardFooter className="bg-white/10 bottom-0 border-t-10 flex justify-between"></CardFooter>
               </Card>
             </Tab>
             <Tab key="Code" className="h-full" title="Code">
@@ -253,9 +321,30 @@ export default function App() {
                     theme={vscodeDark}
                     onChange={(value: string) => {
                       setIsUserEditingCode(true);
-                      setCode(value);
-                    }}
-                  />
+                        setCode(value);
+                      }}
+                      extensions={[
+                        highlightLines(shapeId !== null ? [shapeId + 1] : []), // Highlight lines if shapeId is not null
+                      ]}
+                      />
+                  {/* {code.split("\n").map((line, index) => (
+                    <CodeMirror
+                      key={index}
+                      value={line}
+                      height="100%"
+                      theme={vscodeDark}
+                      onChange={(value: string) => {
+                        setIsUserEditingCode(true);
+                        // Update only the specific line that was edited
+                        const updatedCode = code
+                          .split("\n")
+                          .map((l, i) => (i === index + 1 ? value : l))
+                          .join("\n");
+                        setCode(updatedCode);
+                      }}
+                      className={`cm-editor ${shapeId === index ? "selected-code" : ""}`}
+                    />
+                  ))} */}
                 </CardBody>
                 <CardFooter className="bg-white/10 bottom-0 border-t-10 flex justify-between">
                   <pre>
@@ -282,56 +371,85 @@ export default function App() {
                 <CardBody>
                   <ScrollShadow className="w-full h-[510px]">
                     <div className="text-sm text-white font-mono space-y-2 leading-relaxed pb-4">
-                    <p><span className="font-bold text-gray-300">Starting frame size :</span></p>
-                    <p className="pl-4 text-gray-400 font-medium">
-                      INIT &lt;width&gt; &lt;height&gt;
-                      <br />
-                      <span className="text-gray-400">Ex. INIT 100 100</span>
-                    </p>
-                    <p className="mt-3"><span className="font-bold text-gray-300">Shape drawing command :</span></p>
-                    <p className="pl-4 text-gray-400 font-medium">
-                      LIN &lt;x1&gt; &lt;y1&gt; &lt;x2&gt; &lt;y2&gt;
-                      <br />
-                      <span className="text-gray-400">Ex. LIN 15 10 55 60</span>
-                    </p>
-                    <p className="pl-4 text-gray-400 font-medium">
-                      CIR &lt;center x&gt; &lt;center y&gt; &lt;radius&gt;
-                      <br />
-                      <span className="text-gray-400">Ex. CIR 50 50 10</span>
-                    </p>
-                    <p className="pl-4 text-gray-400 font-medium">
-                      ELI &lt;center x&gt; &lt;center y&gt; &lt;a&gt; &lt;b&gt;
-                      <br />
-                      <span className="text-gray-400">Ex. ELI 30 30 10 20</span>
-                    </p>
-                    <p className="pl-4 text-gray-400 font-medium">
-                      REC &lt;x1&gt; &lt;y1&gt; &lt;x2&gt; &lt;y2&gt;
-                      <br />
-                      <span className="text-gray-400">Ex. REC 10 10 55 60</span>
-                    </p>
-                    <p className="pl-4 text-gray-400 font-medium">
-                      HER &lt;x1&gt; &lt;y1&gt; &lt;x2&gt; &lt;y2&gt; &lt;R1x&gt; &lt;R1y&gt; &lt;R2x&gt; &lt;R2y&gt;
-                      <br />
-                      <span className="text-gray-400">Ex. HER 1 1 90 80 25 35 75 85</span>
-                    </p>
-                    <p className="pl-4 text-gray-400 font-medium">
-                      BEZ &lt;x0&gt; &lt;y0&gt; &lt;x1&gt; &lt;y1&gt; &lt;x2&gt; &lt;y2&gt; &lt;x3&gt; &lt;y3&gt;
-                      <br />
-                      <span className="text-gray-400">Ex. BEZ 20 20 85 95 50 35 15 10</span>
-                    </p>
+                      <p>
+                        <span className="font-bold text-gray-300">
+                          Starting frame size :
+                        </span>
+                      </p>
+                      <p className="pl-4 text-gray-400 font-medium">
+                        INIT &lt;width&gt; &lt;height&gt;
+                        <br />
+                        <span className="text-gray-400">Ex. INIT 100 100</span>
+                      </p>
+                      <p className="mt-3">
+                        <span className="font-bold text-gray-300">
+                          Shape drawing command :
+                        </span>
+                      </p>
+                      <p className="pl-4 text-gray-400 font-medium">
+                        LIN &lt;x1&gt; &lt;y1&gt; &lt;x2&gt; &lt;y2&gt;
+                        <br />
+                        <span className="text-gray-400">
+                          Ex. LIN 15 10 55 60
+                        </span>
+                      </p>
+                      <p className="pl-4 text-gray-400 font-medium">
+                        CIR &lt;center x&gt; &lt;center y&gt; &lt;radius&gt;
+                        <br />
+                        <span className="text-gray-400">Ex. CIR 50 50 10</span>
+                      </p>
+                      <p className="pl-4 text-gray-400 font-medium">
+                        ELI &lt;center x&gt; &lt;center y&gt; &lt;a&gt;
+                        &lt;b&gt;
+                        <br />
+                        <span className="text-gray-400">
+                          Ex. ELI 30 30 10 20
+                        </span>
+                      </p>
+                      <p className="pl-4 text-gray-400 font-medium">
+                        REC &lt;x1&gt; &lt;y1&gt; &lt;x2&gt; &lt;y2&gt;
+                        <br />
+                        <span className="text-gray-400">
+                          Ex. REC 10 10 55 60
+                        </span>
+                      </p>
+                      <p className="pl-4 text-gray-400 font-medium">
+                        HER &lt;x1&gt; &lt;y1&gt; &lt;x2&gt; &lt;y2&gt;
+                        &lt;R1x&gt; &lt;R1y&gt; &lt;R2x&gt; &lt;R2y&gt;
+                        <br />
+                        <span className="text-gray-400">
+                          Ex. HER 1 1 90 80 25 35 75 85
+                        </span>
+                      </p>
+                      <p className="pl-4 text-gray-400 font-medium">
+                        BEZ &lt;x0&gt; &lt;y0&gt; &lt;x1&gt; &lt;y1&gt;
+                        &lt;x2&gt; &lt;y2&gt; &lt;x3&gt; &lt;y3&gt;
+                        <br />
+                        <span className="text-gray-400">
+                          Ex. BEZ 20 20 85 95 50 35 15 10
+                        </span>
+                      </p>
 
-                    <p className="mt-3"><span className="font-bold text-gray-300">Custom color and border :</span></p>
-                    <p className="pl-4 text-gray-400 font-medium">
-                      FIL &lt;colorname&gt;
-                      <br />
-                      <span className="text-gray-400">Ex. CIR 50 50 10 FIL red</span>
-                    </p>
-                    <p className="pl-4 text-gray-400 font-medium">
-                      BOR &lt;stroke width&gt;
-                      <br />
-                      <span className="text-gray-400">Ex. REC 10 10 55 60 BOR 4</span>
-                    </p>
-                  </div>
+                      <p className="mt-3">
+                        <span className="font-bold text-gray-300">
+                          Custom color and border :
+                        </span>
+                      </p>
+                      <p className="pl-4 text-gray-400 font-medium">
+                        FIL &lt;colorname&gt;
+                        <br />
+                        <span className="text-gray-400">
+                          Ex. CIR 50 50 10 FIL red
+                        </span>
+                      </p>
+                      <p className="pl-4 text-gray-400 font-medium">
+                        BOR &lt;stroke width&gt;
+                        <br />
+                        <span className="text-gray-400">
+                          Ex. REC 10 10 55 60 BOR 4
+                        </span>
+                      </p>
+                    </div>
                   </ScrollShadow>
                 </CardBody>
               </Card>
