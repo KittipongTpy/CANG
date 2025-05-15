74f6a64284df9a0cd8f5700abea923d4baada7ee
mapsiir <mp.sryk@gmail.com>
2025-05-15 16:00:34 +0700

    fix: drawdata

--- src/command/render.ts
+++ src/command/render.ts
@@ -10,8 +10,8 @@ export type DrawData =
   | { type: "ellipse"; points: [number, number][]; controlPoints?: [number, number][];color?: string ; strokeWidth?: number ;isFilled?: boolean}
   | { type: "line"; points: [number, number][]; controlPoints?: [number, number][];color?: string ; strokeWidth?: number ;isFilled?: boolean}
   | { type: "rectangle"; points: [number, number][]; controlPoints?: [number, number][]; color?: string; strokeWidth?: number ;isFilled?: boolean}
-  | { type: "bezier"; points: [number, number][]; controlPoints?: [number, number][]; color?: string; strokeWidth?: number}
-  | { type: "hermite"; points: [number, number][]; controlPoints?: [number, number][]; color?: string; strokeWidth?: number};
+  | { type: "bezier"; points: [number, number][]; controlPoints?: [number, number][]; color?: string; strokeWidth?: number;isFilled?: boolean}
+  | { type: "hermite"; points: [number, number][]; controlPoints?: [number, number][]; color?: string; strokeWidth?: number;isFilled?: boolean};
 const commandRegistry: Record<
   string,
   (command: string) => string | null | DrawData
